4e1be7968714d616d6b6f154efa75d373a126538
Michael Edenzon <edenzonmp@g.cofc.edu>
2018-06-20 17:28:58 -0400

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Michael Edenzon on 6/20/18.
+//  Copyright © 2018 Michael Edenzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
